ae0dacd5b1dd830f26ae32deb4d4fb21ab819b86
OscarHagman <oscar.leslie.hagman@gmail.com>
2021-09-30 10:50:19 +0200

    use environment variables

--- configuration/Configuration.js
+++ configuration/Configuration.js
@@ -2,23 +2,32 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 dotenv.config()
+const {
+    DEV_DATABASE_URL,
+    PROD_DATABASE_URL,
+    ENVIRONMENT,
+    PORT
+} = process.env
 
 const connectToDatabase = async () => {
-	try {
-		const DB_URL = process.env.DB_URL
-		await mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-		console.log("Successfully connected to MongoDB")
-	} catch (error) {
-		console.log("Failed to connect to MongoDB", "\nERROR:", error)
-		process.exit()
-	}
+    const DATABASE_URL = ENVIRONMENT === 'DEVELOPMENT' ? DEV_DATABASE_URL : PROD_DATABASE_URL
+    try {
+        await mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+        console.log('✔️  SUCESSFULLY CONNECTED TO DATABASE..')
+    } catch (error) {
+        console.log('❌  ERROR OCCURED WHILE TRYING TO CONNECT TO THE DATABASE..', error.message)
+        process.exit()
+    }
 }
 
-const connectToPort = (app) => {
-    const port = process.env.PORT
-    app.listen(port, () => {
-        console.log(`Server running on port ${port}`)
-    })
+const connectToPort = async (app) => {
+    try {
+        await app.listen(PORT || 3001, () => {
+            console.log(`✔️  SERVER IS RUNNING ON PORT: ${PORT || 3001}`)
+        })
+    } catch (error) {
+        console.log('❌  ERROR OCCURED WHILE TRYING TO CONNECT TO THE PORT..')
+    }
 }
 
 export default {
