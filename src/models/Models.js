bc73235e935514a7592dc1e192284e13f1d66278
OscarHagman <oscar.leslie.hagman@gmail.com>
2021-10-06 16:41:08 +0200

    creates model for File

--- src/models/Models.js
+++ src/models/Models.js
@@ -29,16 +29,25 @@ const LectureMaterialSchema = mongoose.Schema(
 	}, { timestamps: true }
 )
 
+const FileSchema = mongoose.Schema(
+	{
+		file_name: String,
+		document_id: mongoose.Types.ObjectId
+	}, { timestamps: true }
+)
+
 const AssignmetModel = mongoose.model('assignment', AssignmentSchema)
 const ExerciseModel = mongoose.model('exercise', ExerciseSchema)
 const ExaminationModel = mongoose.model('examination', ExaminationSchema)
 const LectureMaterialModel = mongoose.model('lecture material', LectureMaterialSchema)
+const FileModel = mongoose.model("files", FileSchema)
 
 const Models = {
 	AssignmetModel,
 	ExerciseModel,
 	ExaminationModel,
-	LectureMaterialModel
+	LectureMaterialModel,
+	FileModel
 }
 
 export default Models
